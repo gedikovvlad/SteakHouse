9718cd5f748c2c61901bc3d349bfe6346dc05ead
gedikovvlad <143928502+gedikovvlad@users.noreply.github.com>
2023-10-18 20:22:15 +0300

    Steak

---
+++ sctipt.js
@@ -0,0 +1,13 @@
+
+
+const panorama = new PANOLENS.ImagePanorama ('https://images.squarespace-cdn.com/content/v1/54380a61e4b0d17dc4953293/1568738319421-W93W0AJNJJMBY29AVUWY/NewHomeSlides-Taphouse+23x1.jpg?format=2500w');
+let imageContainer = document.querySelector('.image-container')
+
+const viewer = new PANOLENS.Viewer({
+    container: imageContainer,
+    autoRotate: true,
+    autoRotateSpeed: 0.3,
+    controlBar: true,
+});
+viewer.add( panorama );
+container.append(img)
